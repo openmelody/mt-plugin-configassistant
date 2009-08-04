7887c26c8929e20180a20ac47a6df9a9d96b96a6
Byrne Reese <byrne@majordojo.com>
2009-08-04 00:08:55 -0700

    * Added support for radio-image - a new custom field type that allows for someone to create a range of graphical images to select from. Perfect for example for layout selection. * Moved js and css to static files.

---
+++ mt-static/plugins/ConfigAssistant/app.js
@@ -0,0 +1,25 @@
+$(document).ready( function() {
+  var active = $('#content-nav ul li.active a').attr('title');
+  $('#'+active).show();
+  $('h2#page-title span').html( $('#content-nav ul li.active a b').html() );
+  $('#fieldsets input, #fieldsets select, #fieldsets textarea').change( function () {
+    var changed = $(this).parent().parent().parent().attr('id');
+    $('#content-nav ul li.'+changed).addClass('changed');
+  });
+  $('#content-nav ul li a').click( function() {
+    var newactive = $(this).attr('title');
+    $('#content-nav li.active').removeClass('active');
+    $('#' + active).hide();
+    $('#content-nav li.' + newactive).addClass('active');
+    $('#' + newactive).show();
+    $('h2#page-title span').html( $('#content-nav ul li.'+newactive+' a b').html() );
+    active = newactive;
+  });  
+  $('.field-type-radio-image li input:checked').each( function() { $(this).parent().addClass('selected'); });
+  $('.field-type-radio-image li').click( function() {
+    $(this).parent().find('input:checked').attr('checked',false);
+    $(this).find('input').attr('checked',true);
+    $(this).parent().find('.selected').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
