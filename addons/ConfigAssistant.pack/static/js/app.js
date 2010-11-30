ec8ca2093134b9257757d7d37c718cd52b7129a6
Byrne <byrne@majordojo.com>
2010-11-30 09:37:53 -0800

    Dramatically improved javascript/history state management in the same way done for Theme Manager. Using jquery.history.js plugin. Awesome.

--- addons/ConfigAssistant.pack/static/js/app.js
+++ addons/ConfigAssistant.pack/static/js/app.js
@@ -1,19 +1,19 @@
 $(document).ready( function() {
-  var active = $('#content-nav ul li.active a').attr('id');
-  $('#' + active + '-content').show();
   $('h2#page-title span').html( $('#content-nav ul li.active a b').html() );
   $('#fieldsets input, #fieldsets select, #fieldsets textarea').change( function () {
     var changed = $(this).parent().parent().parent().attr('id');
     $('#content-nav ul li.'+changed).addClass('changed');
   });
   $('#content-nav ul li a').click( function() {
-    var newactive = $(this).attr('id');
+    var active    = $(this).parents('ul').find('li.active a').attr('id').replace(/-tab$/,'');
+    var newactive = $(this).attr('id').replace(/-tab$/,'');
     $('#content-nav li.active').removeClass('active');
-    $('#' + active + '-content').hide();
-    $('#content-nav li.' + newactive).addClass('active');
-    $('#' + newactive + '-content').show();
-    $('h2#page-title span').html( $('#content-nav ul li.'+newactive+' a b').html() );
-    active = newactive;
+    $('#' + active + '-tab-content').hide();
+    $('#content-nav li.' + newactive+'-tab').addClass('active');
+    $('#' + newactive + '-tab-content').show();
+    $('h2#page-title span').html( $('#content-nav ul li.'+newactive+'-tab a b').html() );
+    document.title = $(this).attr('title');
+    window.location.hash = newactive;
   });
   $('.field-type-radio-image li input:checked').each( function() { $(this).parent().addClass('selected'); });
   $('.field-type-radio-image li').click( function() {
@@ -23,5 +23,11 @@ $(document).ready( function() {
     $(this).addClass('selected');
     var changed = $(this).parent().parent().parent().parent().attr('id');
     $('#content-nav ul li.'+changed).addClass('changed');
+  });
+  $.history.init(function(hash){
+    if (hash == "") {
+        hash = $('#content-nav ul li:first-child a').attr('id').replace(/-tab$/,'');
+    }
+    $('#content-nav ul li.'+hash+'-tab a').click();
   });
 });
